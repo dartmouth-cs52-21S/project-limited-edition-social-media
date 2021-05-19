15fd2a3fd35f240ff1db114900bc609fa04a56df
mackerson123 <maxwell.g.reiferson.22@dartmouth.edu>
2021-05-19 14:10:36 -0400

    react-native sa clone

--- navigation/main_tab_bar.js
+++ navigation/main_tab_bar.js
@@ -2,38 +2,23 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/FontAwesome';
-import About from '../components/about';
-import SearchTab from './search_tab';
+
+const AboutTab = (props) => {
+  return <View style={{ flex: 1, justifyContent: 'center' }}><Text>about</Text></View>;
+};
+
+const SearchTab = (props) => {
+  return <View style={{ flex: 1, justifyContent: 'center' }}><Text>Search</Text></View>;
+};
 
 const Tab = createBottomTabNavigator();
 
 const MainTabBar = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Search"
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused }) => {
-            let iconName;
-
-            // Customize the icon we display based on the tab route
-            if (route.name === 'About') {
-              iconName = 'info-circle';
-            }
-
-            // Adding the search icon
-            else if (route.name === 'Search') {
-              iconName = 'search';
-            }
-
-            // Return the respective icon
-            return <Ionicons name={iconName} size={26} color={focused ? '#58AADA' : 'grey'} />;
-          },
-        })}
-      >
+      <Tab.Navigator>
         <Tab.Screen name="Search" component={SearchTab} />
-        <Tab.Screen name="About" component={About} />
+        <Tab.Screen name="About" component={AboutTab} />
       </Tab.Navigator>
     </NavigationContainer>
   );
