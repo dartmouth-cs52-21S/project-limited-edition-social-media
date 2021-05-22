ca1bb1b3dc6bc3e4bfacfe63b576220dd3164f2e
mackerson123 <maxwell.g.reiferson.22@dartmouth.edu>
2021-05-22 13:13:28 -0400

    added authreducer, linked to reducers/index.js

---
+++ reducers/auth-reducer.js
@@ -0,0 +1,20 @@
+import { ActionTypes } from '../actions';
+
+const initialState = {
+  authenticated: false,
+};
+
+const AuthReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionTypes.AUTH_USER:
+      return { ...state, authenticated: true };
+    case ActionTypes.DEAUTH_USER:
+      return { ...state, authenticated: false };
+    case ActionTypes.AUTH_ERROR:
+      return { ...state, authenticated: false };
+    default:
+      return state;
+  }
+};
+
+export default AuthReducer;
--- reducers/index.js
+++ reducers/index.js
@@ -2,9 +2,11 @@
 // this defines what your store state will look like
 import { combineReducers } from 'redux';
 import PostsReducer from './post-reducer';
+import AuthReducer from './auth-reducer';
 
 const rootReducer = combineReducers({
   posts: PostsReducer,
+  auth: AuthReducer,
 });
 
 export default rootReducer;
