22889adcb38cb4563bb8ae48280b13af7d7500b3
Irene Lam <irene.x.lam.22@dartmouth.edu>
2021-05-30 01:03:54 -0400

    Readded paper package, reverted async

--- actions/index.js
+++ actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import { AsyncStorage } from 'react-native';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
+// import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // keys for actiontypes
 export const ActionTypes = {
