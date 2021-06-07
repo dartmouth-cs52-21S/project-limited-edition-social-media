89961ea6eb3662f39ca31997132f5fc138935a3a
Irene Lam <irene.x.lam.22@dartmouth.edu>
2021-06-07 12:25:16 -0400

    Debounce

--- components/search.js
+++ components/search.js
@@ -21,13 +21,15 @@ class Search extends Component {
     this.onChangeSearch = this.onChangeSearch.bind(this);
     this.renderUser = this.renderUser.bind(this);
     this.renderUserProfile = this.renderUserProfile.bind(this);
+    this.onIconPress = this.onIconPress.bind(this);
   }
 
   componentDidMount() {
     this.setState(DEFAULT_STATE);
   }
 
-  onChangeSearch(searchQuery) {
+  onIconPress() {
+    const { searchQuery } = this.state;
     if (!searchQuery) {
       this.setState(DEFAULT_STATE);
       return;
@@ -36,6 +38,10 @@ class Search extends Component {
     getSearchedUsers(searchQuery).then(({ data: users }) => this.setState({
       searchQuery, users,
     }));
+  }
+
+  onChangeSearch(searchQuery) {
+    this.setState(searchQuery ? { searchQuery } : DEFAULT_STATE);
   }
 
   renderUserProfile({ username }) {
@@ -65,11 +71,12 @@ class Search extends Component {
         <Searchbar
           placeholder="Find User"
           onChangeText={this.onChangeSearch}
+          onIconPress={this.onIconPress}
           value={this.state?.searchQuery || ''}
         />
 
         <List.Section style={styles.listSection}>
-          <List.Subheader>Search by username</List.Subheader>
+          <List.Subheader>Press enter to search by username</List.Subheader>
           {(this.state?.users || []).map(this.renderUser)}
         </List.Section>
       </SafeAreaView>
